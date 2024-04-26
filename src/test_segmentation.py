a11a6871148e5f7258599e977c0620d768cabce5
javirk <jgamtejero@gmail.com>
2024-04-26 10:58:32 +0200

    Save output masks

--- src/test_segmentation.py
+++ src/test_segmentation.py
@@ -49,7 +49,7 @@ def testing(args):
             # results_bb = eval_single_dataset(args, model, dataset, prompting='bb', embedding=False)
             # results_point = eval_single_dataset(args, model, dataset, prompting='point', embedding=False)
 
-            write_results(args, [results_per_tile], f'{dataset_name}_{fold_num}')
+            write_results(args, [results_per_tile], f'{dataset_name}')
 
             results = {**results_none}
 
