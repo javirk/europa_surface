343444c59fa835818e341bf7bf492c72ef77f310
javirk <jgamtejero@gmail.com>
2024-06-05 15:27:24 +0200

    Hyperparam search

--- scripts/hyperparam_search/arr.sh
+++ scripts/hyperparam_search/arr.sh
@@ -3,7 +3,6 @@
 # You must specify a valid email address!
 #SBATCH --mail-user=javier.gamazo-tejero@unibe.ch
 #SBATCH --mail-type=FAIL,END
-#SBATCH --account=ws_00000
 
 # Partition
 #SBATCH --partition=gpu-invest # all, gpu, phi, long
@@ -13,7 +12,7 @@
 #SBATCH --mem-per-cpu=4G
 
 # on gpu partition
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:rtx3090:1
 
 # maximum cores is 20 on all, 10 on long, 24 on gpu, 64 on phi!
 #SBATCH --cpus-per-task=8
@@ -23,7 +22,7 @@
 
 #SBATCH --job-name=point_prompting_arr
 #SBATCH --output=logs/slurm-%A.out
-#SBATCH --array=1-240%2
+#SBATCH --array=1-30%2
 
 # Replace <number_of_scripts> with the total number of script files you have.
 
