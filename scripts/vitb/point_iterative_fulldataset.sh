7ea84c9d7326d11c03229ea1d2e047608c2eb1ae
javirk <jgamtejero@gmail.com>
2024-06-07 10:32:29 +0200

    Training scripts vitb

--- scripts/vitb/point_iterative_fulldataset.sh
+++ scripts/vitb/point_iterative_fulldataset.sh
@@ -37,7 +37,7 @@
 ##SBATCH --array=0-11%8
 
 # Main Python code below this line
-PYTHONPATH="./" python main.py --batch-size=128 --lr=8e-4 --wd=0.01 --epochs=400 --workers 8 \
+PYTHONPATH="./" python main.py --batch-size=128 --lr=1e-4 --wd=0.01 --epochs=400 --workers 8 \
   --data-location=/storage/workspaces/artorg_aimi/ws_00000/javier/datasets/europa/ \
   --eval-datasets=GalileoDataset --train-dataset=Galileo --loss-fn=DiceLoss,FocalLoss,IoUHeadLoss \
   --loss-weights=1.0,20.0,1.0 --wandb --exp-name=Galileo --save=./results/ --pretrained-model=./ckpts/vitb_112.pt \
